900fccdf704ce642af16e5349ce1594c7f970dfe
sabriny <sabrinyneres12@gmail.com>
2022-11-17 11:10:55 -0300

    implementação das funções de somar e subtair.

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 function exibirMsg () {
     alert('sabriny alerta');
 }
+function somar(v1,v2) {
+    return parserFloat(v1) + parseFloat(v2);
+}
+function subtrair(v1,v2) {
+    return parserFloat(v1) - parseFloat(v2);
+}
